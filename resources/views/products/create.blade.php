a4377a7a52d859200567004eb37bd6972edf44e6
AndreHilario <andre18hilario@gmail.com>
2023-11-23 11:28:48 -0300

    feat: add correct hmtl to table on index and erros on create

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 <body>
-    <h1>Create new product</h1>
+    <h1>Criar produto</h1>
+
+    <div>
+        @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+        
+        @endif
+
+    </div>
+
     <form method="post" action="{{route('product.store')}}">
         @csrf
         @method('post')
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,6 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Products Table</h1>
+    <h1>Produtos</h1>
+    <div>
+        <table border="2">
+            <thead>
+                <tr>
+                    <th>Código de Identificação</th>
+                    <th>Nome</th>
+                    <th>Link da Imagem</th>
+                    <th>Preço</th>
+                    <th>CEP do Galpão</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+                    <tr>
+                        <td>{{ $product->id_code }}</td>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->url }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->cep }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
 </body>
 </html>
